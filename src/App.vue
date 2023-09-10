624d07a50c9f47c657593a3adcd2fc9d3a20d210
Константин Чугуров <lerochka.sayfullina@mail.ru>
2023-09-11 00:40:39 +0500

    1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-btn variant="flat" color="teal-darken-3">give </v-btn>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
